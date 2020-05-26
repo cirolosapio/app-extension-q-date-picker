f21ded6613a8df77ac2715969494cd7a638215bf
Ciro Laptop <lscorionjs@gmail.com>
2020-05-26 15:39:51 +0200

    update quasar/eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,6 @@ module.exports = {
 
     'standard',
 
-
     'plugin:quasar/standard'
   ],
 
--- src/index.js
+++ src/index.js
@@ -8,17 +8,13 @@
 const extendConf = function (conf) {
   // make sure qfilter boot file is registered
   conf.boot.push('~quasar-app-extension-q-date-filter/src/boot/q-date-filter.js')
-  console.log(` App Extension (q-date-filter) Info: 'Adding q-date-filter boot reference to your quasar.conf.js'`)
+  console.log('App Extension (q-date-filter) Info: \'Adding q-date-filter boot reference to your quasar.conf.js\'')
 
   // make sure boot & component files transpile
   conf.build.transpileDependencies.push(/quasar-app-extension-q-date-filter[\\/]src/)
 }
 
 module.exports = function (api) {
-  // quasar compatibility check
-  api.compatibleWith('quasar', '^1.0.0')
-  api.compatibleWith('@quasar/app', '^1.0.0')
-
   // register JSON api
   api.registerDescribeApi('QDateFilter', './component/QDateFilter.json')
 
